cbe96a2c660fced4f8d169590c76032a892c3351
Artem <griukov@protonmail.com>
2023-03-22 23:15:53 +0200

    Parsing commands from bytes

--- apps/basket-server/src/main.rs
+++ apps/basket-server/src/main.rs
@@ -4,11 +4,44 @@ use std::{
 };
 
 use anyhow::Result;
+use bytecheck::CheckBytes;
 use log::info;
+use rkyv::{Archive, Deserialize, Serialize};
 
-const BUFFER_CAPACITY: u16 = std::u16::MAX;
+type BufferCapacity = u16;
+const BUFFER_CAPACITY: BufferCapacity = BufferCapacity::MAX;
 const HOST: &str = "0.0.0.0";
 const PORT: u16 = 3000;
+
+type Buffer = [u8; BUFFER_CAPACITY as usize];
+
+type Key = u32;
+
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+#[archive(compare(PartialEq))]
+#[archive_attr(derive(CheckBytes, Debug))]
+enum Value {
+    INT(i32),
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+#[archive(compare(PartialEq))]
+#[archive_attr(derive(CheckBytes, Debug))]
+enum Action {
+    SET(Key, Value),
+    GET(Key),
+    DEL(Key),
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+// This will generate a PartialEq impl between our unarchived and archived types
+#[archive(compare(PartialEq))]
+// To use the safe API, you have to derive CheckBytes for the archived type
+#[archive_attr(derive(CheckBytes, Debug))]
+struct Command {
+    action: Action,
+    timestamp: u128,
+}
 
 fn main() -> Result<()> {
     env_logger::init();
@@ -21,17 +54,28 @@ fn main() -> Result<()> {
             info!("New connection {:?}", stream);
 
             read_command_from_stream(&mut stream, &mut buffer)?;
+            let command = parse_command(&buffer);
+
+            println!("Command {:#?}", command);
         }
     }
 
     Ok(())
 }
 
-fn read_command_from_stream(stream: &mut TcpStream, buffer: &mut [u8]) -> std::io::Result<()> {
+fn parse_command(buffer: &Buffer) -> Result<Command> {
+    let archived = unsafe { rkyv::archived_root::<Command>(&buffer[..]) };
+
+    let command: Command = archived.deserialize(&mut rkyv::Infallible)?;
+
+    Ok(command)
+}
+
+fn read_command_from_stream(stream: &mut TcpStream, buffer: &mut Buffer) -> std::io::Result<()> {
     let mut size_bytes = [0; 2];
     stream.read(&mut size_bytes)?;
 
-    let len = u16::from_be_bytes(size_bytes);
+    let len = BufferCapacity::from_be_bytes(size_bytes);
 
     if len < BUFFER_CAPACITY {
         return Err(std::io::Error::new(
